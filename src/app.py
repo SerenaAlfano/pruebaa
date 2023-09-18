5a57d573c9944a41864eb196bb9994beb0bc1350
rocio-robledo <142269981+rocio-robledo@users.noreply.github.com>
2023-09-18 15:05:34 -0300

    Cambios varios

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from datetime import datetime
 import os #Permite acceder a los directorios
 import database as db
 
@@ -32,6 +33,7 @@ def inicio():
 
 @app.route("/index")
 def alta(): 
+    form_data = session.pop('form_data', None)  # Obtener los datos del formulario almacenados en sesión
     cursor = db.database.cursor()
     cursor.execute("SELECT * FROM alumnos")
     myresult = cursor.fetchall()
@@ -41,7 +43,7 @@ def alta():
     for record in myresult:
         insertObject .append(dict(zip(columnNames, record)))
     cursor.close()
-    return render_template("index.html", data = insertObject)
+    return render_template("index.html", data = insertObject, form_data=form_data)
 
 @app.route("/alumnos", methods=["POST"])
 def agregarAlumno():
@@ -59,6 +61,35 @@ def agregarAlumno():
     dia = request.form["dia"]
     horario = request.form["horario"]
     materia = request.form["materia"]
+  
+     # Obtén la fecha de nacimiento del formulario
+    fecha_nacimiento_str = request.form["fecha_nacimiento"]
+    fecha_nacimiento = datetime.strptime(fecha_nacimiento_str, "%Y-%m-%d").date()
+
+    # Obtiene la fecha actual
+    fecha_actual = datetime.now().date()
+
+    # Compara la fecha de nacimiento con la fecha actual
+    
+    if fecha_nacimiento >= fecha_actual:
+        flash("La fecha de nacimiento debe ser en el pasado.", "error")
+        session['form_data'] = {
+        'nombre': nombre,
+        'apellido': apellido,
+        'email': email,
+        'telefono': telefono,
+        'fecha_nacimiento': fecha_nacimiento_str,
+        'fecha_inicio': fecha_inicio,
+        'colegio': colegio,
+        'curso':  curso,
+        'nivel_educativo':nivel_educativo,
+        'nombre_titular':nombre_titular,
+        'telefono_titular':telefono_titular,
+        'dia':dia,
+        'horario':horario,
+        'materia':materia
+        }
+        return redirect(url_for('alta'))
 
 
     if nombre and apellido and email and telefono and fecha_nacimiento and fecha_inicio and colegio and curso and nivel_educativo and nombre_titular and telefono_titular and dia and horario and materia:
@@ -97,6 +128,35 @@ def editar(id):
     dia = request.form["dia"]
     horario = request.form["horario"]
     materia = request.form["materia"]
+ 
+ # Obtén la fecha de nacimiento del formulario
+    fecha_nacimiento_str = request.form["fecha_nacimiento"]
+    fecha_nacimiento = datetime.strptime(fecha_nacimiento_str, "%Y-%m-%d").date()
+
+    # Obtiene la fecha actual
+    fecha_actual = datetime.now().date()
+
+    # Compara la fecha de nacimiento con la fecha actual
+    
+    if fecha_nacimiento >= fecha_actual:
+        flash("La fecha de nacimiento debe ser en el pasado.", "error")
+        session['form_data'] = {
+        'nombre': nombre,
+        'apellido': apellido,
+        'email': email,
+        'telefono': telefono,
+        'fecha_nacimiento': fecha_nacimiento_str,
+        'fecha_inicio': fecha_inicio,
+        'colegio': colegio,
+        'curso':  curso,
+        'nivel_educativo':nivel_educativo,
+        'nombre_titular':nombre_titular,
+        'telefono_titular':telefono_titular,
+        'dia':dia,
+        'horario':horario,
+        'materia':materia
+        }
+        return redirect(url_for('alta'))
 
     if nombre and apellido and email and telefono and fecha_nacimiento and fecha_inicio and colegio and curso and nivel_educativo and nombre_titular and telefono_titular and dia and horario and materia:
         cursor = db.database.cursor()
@@ -111,6 +171,14 @@ def editar(id):
 #CargaDatos
 @app.route("/ingresos", methods=["GET", "POST"])
 def ingresos():
+    form_data = session.pop('form_data', None)  # Obtener los datos del formulario almacenados en sesión
+     # Define fecha_pago with a default value (it can be None or any other appropriate default)
+    fecha_pago = None
+    fecha_actual = None
+    nombre_alumno= None
+    apellido_alumno= None
+    monto= None
+    medios_de_pago = None
     if request.method == "POST":
         # Tu lógica para agregar datos aquí
         nombre_alumno = request.form["nombre_alumno"]
@@ -118,14 +186,33 @@ def ingresos():
         fecha_pago = request.form["fecha_pago"]
         monto = request.form["monto"]
         medios_de_pago = request.form["medios_de_pago"]
+        
+        # Obtén la fecha de pago del formulario
+        fecha_pago_str = request.form["fecha_pago"]
+        fecha_pago = datetime.strptime(fecha_pago_str, "%Y-%m-%d").date()
+        
+        # Obtiene la fecha actual
+        fecha_actual = datetime.now().date()
 
-        if nombre_alumno and apellido_alumno and fecha_pago and monto and medios_de_pago:
-            cursor = db.database.cursor()
-            sql = "INSERT INTO ingresos (nombre_alumno, apellido_alumno, fecha_pago, monto, medios_de_pago) VALUES (%s, %s, %s, %s, %s)"
-            data = (nombre_alumno, apellido_alumno, fecha_pago, monto, medios_de_pago)
-            cursor.execute(sql, data)
-            db.database.commit()
-
+    # Compara la fecha de pago con la fecha actual
+    if fecha_pago is not None and fecha_actual is not None and fecha_pago > fecha_actual:
+        flash("La fecha de pago debe ser actual o anterior.", "error")
+        session['form_data'] = {
+        'nombre_alumno': nombre_alumno,
+        'apellido_alumno': apellido_alumno,
+        'fecha_pago': fecha_pago_str,
+        'monto':monto,
+        'medios_de_pago':medios_de_pago
+        }
+        return redirect(url_for('ingresos'))
+    
+    if nombre_alumno and apellido_alumno and fecha_pago and monto and medios_de_pago:
+        cursor = db.database.cursor()
+        sql = "INSERT INTO ingresos (nombre_alumno, apellido_alumno, fecha_pago, monto, medios_de_pago) VALUES (%s, %s, %s, %s, %s)"
+        data = (nombre_alumno, apellido_alumno, fecha_pago, monto, medios_de_pago)
+        cursor.execute(sql, data)
+        db.database.commit()
+        
         return redirect(url_for('ingresos'))  # Redirige a la misma vista después de agregar datos
 
 
@@ -140,7 +227,7 @@ def ingresos():
         insertObject.append(dict(zip(columnNames, record)))
     cursor.close()
     
-    return render_template("ingresos.html", data=insertObject)
+    return render_template("ingresos.html", data=insertObject,form_data=form_data)
 
 
 
@@ -155,42 +242,97 @@ def eliminar_ingresos(id_ingresos):
     return redirect(url_for('ingresos'))
 
 
-#Actualizar
-@app.route("/editaringresos/<int:id_ingresos>", methods = ["POST"])
+# Actualizar ingresos
+@app.route("/editaringresos/<int:id_ingresos>", methods=["POST", "GET"])
 def editaringresos(id_ingresos):
-    nombre_alumno = request.form["nombre_alumno"]
-    apellido_alumno = request.form["apellido_alumno"]
-    fecha_pago = request.form["fecha_pago"]
-    monto = request.form["monto"]
-    medios_de_pago = request.form["medios_de_pago"]
+    registro_editar = None  # Define registro_editar inicialmente como None
 
-    if nombre_alumno and apellido_alumno and fecha_pago and monto and medios_de_pago:
+    if request.method == "GET":
+        cursor = db.database.cursor()
+        cursor.execute("SELECT nombre_alumno, apellido_alumno, fecha_pago, monto, medios_de_pago FROM ingresos WHERE id_ingresos = %s", (id_ingresos,))
+        registro_editar = cursor.fetchone()
+        cursor.close()
+        data=db.database
+        if registro_editar:
+            return render_template("editar_ingresos.html", registro=registro_editar, data=data)
+        else:
+            # Manejar el caso si no se encuentra el registro a editar
+            flash("Registro no encontrado.", "error")
+            return redirect(url_for('ingresos'))
+
+    if request.method == "POST":
+        nombre_alumno = request.form["nombre_alumno"]
+        apellido_alumno = request.form["apellido_alumno"]
+        fecha_pago_str = request.form["fecha_pago"]
+        fecha_pago = datetime.strptime(fecha_pago_str, "%Y-%m-%d").date()
+        monto = request.form["monto"]
+        medios_de_pago = request.form["medios_de_pago"]
+
+        # Obtiene la fecha actual
+        fecha_actual = datetime.now().date()
+
+        # Compara la fecha de pago con la fecha actual
+        if fecha_pago is not None and fecha_actual is not None and fecha_pago > fecha_actual:
+            flash("La fecha de pago debe ser actual o anterior.", "error")
+            session['form_data'] = {
+                'nombre_alumno': nombre_alumno,
+                'apellido_alumno': apellido_alumno,
+                'fecha_pago': fecha_pago_str,
+                'monto': monto,
+                'medios_de_pago': medios_de_pago
+            }
+            return redirect(url_for('editaringresos', id_ingresos=id_ingresos))
+        
+        if nombre_alumno and apellido_alumno and fecha_pago and monto and medios_de_pago:
             cursor = db.database.cursor()
             sql = "UPDATE ingresos SET nombre_alumno = %s, apellido_alumno = %s, fecha_pago = %s, monto = %s, medios_de_pago = %s WHERE id_ingresos = %s"
             data = (nombre_alumno, apellido_alumno, fecha_pago, monto, medios_de_pago, id_ingresos)
             cursor.execute(sql, data)
             db.database.commit()
-
-    return redirect(url_for('ingresos'))
-
+            data=db.database
+        return redirect(url_for('ingresos'))
 
 
 #egresos
 @app.route("/egresos", methods=["GET", "POST"])
 def egresos():
+    form_data = session.pop('form_data', None)  # Obtener los datos del formulario almacenados en sesión
+    fecha_pago = None
+    fecha_actual = None
+    servicios= None
+    monto= None
+    medios_de_pago = None
     if request.method == "POST":
         # Tu lógica para agregar datos aquí
         servicios = request.form["servicios"]
         fecha_pago = request.form["fecha_pago"]
         monto = request.form["monto"]
         medios_de_pago = request.form["medios_de_pago"]
+        
+         # Obtén la fecha de pago del formulario
+        fecha_pago_str = request.form["fecha_pago"]
+        fecha_pago = datetime.strptime(fecha_pago_str, "%Y-%m-%d").date()
+        
+         # Obtiene la fecha actual
+        fecha_actual = datetime.now().date()
 
-        if servicios and fecha_pago and monto and medios_de_pago:
-            cursor = db.database.cursor()
-            sql = "INSERT INTO egresos (servicios, fecha_pago, monto, medios_de_pago) VALUES (%s, %s, %s, %s)"
-            data = (servicios, fecha_pago, monto, medios_de_pago)
-            cursor.execute(sql, data)
-            db.database.commit()
+    # Compara la fecha de pago con la fecha actual
+    if fecha_pago is not None and fecha_actual is not None and fecha_pago > fecha_actual:
+        flash("La fecha de pago debe ser actual o anterior.", "error")
+        session['form_data'] = {
+        'servicios': servicios,
+        'fecha_pago': fecha_pago_str,
+        'monto':monto,
+        'medios_de_pago':medios_de_pago
+        }
+        return redirect(url_for('egresos'))
+
+    if servicios and fecha_pago and monto and medios_de_pago:
+        cursor = db.database.cursor()
+        sql = "INSERT INTO egresos (servicios, fecha_pago, monto, medios_de_pago) VALUES (%s, %s, %s, %s)"
+        data = (servicios, fecha_pago, monto, medios_de_pago)
+        cursor.execute(sql, data)
+        db.database.commit()
         return redirect(url_for('egresos'))  # Redirige a la misma vista después de agregar datos
 
 # Tu lógica para mostrar la página de egresos con la lista de datos aquí
@@ -203,8 +345,8 @@ def egresos():
     for record in myresult:
         insertObject.append(dict(zip(columnNames, record)))
     cursor.close()
-    
-    return render_template("egresos.html", data=insertObject)
+    data=db.database
+    return render_template("egresos.html", data=insertObject,form_data=form_data)
 
 
 
@@ -220,20 +362,51 @@ def eliminar_egresos(id):
 
 
 #Actualizar
-@app.route("/editaregresos/<int:id>", methods = ["POST"])
+@app.route("/editaregresos/<int:id>", methods=["POST", "GET"])
 def editaregresos(id):
-    servicios = request.form["servicios"]
-    fecha_pago = request.form["fecha_pago"]
-    monto = request.form["monto"]
-    medios_de_pago = request.form["medios_de_pago"]
+    registro_editar = None  # Define registro_editar inicialmente como None
 
+    if request.method == "GET":
+        cursor = db.database.cursor()
+        cursor.execute("SELECT servicios, fecha_pago, monto, medios_de_pago FROM egresos WHERE id = %s", (id,))
+        registro_editar = cursor.fetchone()
+        cursor.close()
+        data=db.database
+
+        if registro_editar:
+            return render_template("editar_egresos.html", registro=registro_editar,data=data)
+        else:
+            # Manejar el caso si no se encuentra el registro a editar
+            flash("Registro no encontrado.", "error")
+            return redirect(url_for('egresos'))
+
+    if request.method == "POST":
+        servicios = request.form["servicios"]
+        fecha_pago_str = request.form["fecha_pago"]
+        fecha_pago = datetime.strptime(fecha_pago_str, "%Y-%m-%d").date()
+        monto = request.form["monto"]
+        medios_de_pago = request.form["medios_de_pago"]
+
+        # Obtiene la fecha actual
+        fecha_actual = datetime.now().date()
+
+    # Compara la fecha de pago con la fecha actual
+    if fecha_pago is not None and fecha_actual is not None and fecha_pago > fecha_actual:
+        flash("La fecha de pago debe ser actual o anterior.", "error")
+        session['form_data'] = {
+        'servicios': servicios,
+        'fecha_pago': fecha_pago_str,
+        'monto':monto,
+        'medios_de_pago':medios_de_pago
+        }
+        return redirect(url_for('egresos'))
     if servicios and fecha_pago and monto and medios_de_pago:
             cursor = db.database.cursor()
             sql = "UPDATE egresos SET servicios = %s, fecha_pago = %s, monto = %s, medios_de_pago = %s WHERE id = %s"
             data = (servicios, fecha_pago, monto, medios_de_pago, id)
             cursor.execute(sql, data)
             db.database.commit()
-
+            data=db.database
     return redirect(url_for('egresos'))
 
 
