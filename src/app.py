6229d7b861e001145b26f42850e95e5ddcbe578f
SerenaAlfano <Salfano@fase4.net.ar>
2023-09-25 14:09:04 -0300

    Revert "Merge branch 'main' into rama-rocio"
    This reverts commit b6a4395c15d0fe25cdd81fc65949bf43cc0aa126, reversing
    changes made to ba9cd4dbf3c371d002020fe24015edcbbc55f506.

--- src/app.py
+++ src/app.py
@@ -12,7 +12,6 @@ from flask_paginate import Pagination, get_page_parameter
 import re
 
 
-
 template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 template_dir = os.path.join(template_dir,"src", "templates")
 
@@ -772,25 +771,7 @@ def descargar_pdf(id_ingresos):
     return response
 
 
-@app.route('/resumen')
-def resumen():
-    form_data = session.pop('form_data', None)  # Obtener los datos del formulario almacenados en sesión
 
-    # Calcular el total de ingresos
-    cursor = db.database.cursor()
-    cursor.execute("SELECT SUM(monto) FROM ingresos")
-    total_ingresos = cursor.fetchone()[0] or 0
-    cursor.close()
-
-    # Calcular el total de egresos
-    cursor = db.database.cursor()
-    cursor.execute("SELECT SUM(monto) FROM egresos")
-    total_egresos = cursor.fetchone()[0] or 0
-    cursor.close()
-
-    saldo = total_ingresos - total_egresos
-
-    return render_template("resumen.html", total_ingresos=total_ingresos, total_egresos=total_egresos, saldo=saldo, form_data=form_data)
 
 app.secret_key = 'veronica'
 if __name__ == "__main__":
