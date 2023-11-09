cc04b7bb4ace303836c3dd89e8e98c9cfb270f69
SerenaAlfano <Salfano@fase4.net.ar>
2023-11-09 16:50:43 -0300

    cam

--- src/datoseventos.php
+++ src/datoseventos.php
@@ -9,61 +9,122 @@ require("conexion.php");
 $conexion = regresarConexion();
 
 switch($_GET['accion']) {
- //Listar evento
     case 'listar':
-        $datos = mysqli_query($conexion, "select id, 
-        nombre_alumno as title,
-        inicio as start,
+        $datos = mysqli_query($conexion,"select id,
+        titulo as title, 
         descripcion, 
+        inicio as start, 
+        fin as end,
         colortexto as textColor,
-        colorfondo as backgroundColor
+        colorfondo as backgroundColor 
         from eventos");
 
-        $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
-        echo json_encode($resultado);      
-        break;
-
- //Agregar evento
-    case 'agregar':
-        
-        $nombre_alumno = $_POST['nombre_alumno'];
-        $inicio = $_POST['inicio'] . ' ' . $_POST['horario'] . ':00'; 
-        // Obtener la lista de materias separadas por comas
+        $resultado = mysqli_fetch_all ($datos, MYSQLI_ASSOC);
+        echo json_encode($resultado);
     
-        $respuesta = mysqli_query($conexion, "insert into eventos(nombre_alumno,inicio,descripcion,colortexto,colorfondo) values
-        ('$nombre_alumno','$inicio','$_POST[descripcion]',
-        '$_POST[colortexto]','$_POST[colorfondo]')");
-        echo json_encode($respuesta);    
         break;
-
-// Modificar evento
-case 'modificar':
-    $inicio = $_POST['inicio'] . ' ' . $_POST['horario'] . ':00';
-    $inicio = date('Y-m-d H:i:s', strtotime($inicio));
-
-    $respuesta = mysqli_query($conexion, "UPDATE eventos SET 
-        nombre_alumno = '$_POST[nombre_alumno]',
-        inicio = '$inicio', 
-        descripcion = '$_POST[descripcion]',
-        colortexto = '$_POST[colortexto]',
-        colorfondo = '$_POST[colorfondo]'
-        WHERE id = $_POST[id]");
-    echo json_encode($respuesta);
-    break;
-
-    //Borrar evento
-    case 'borrar':
-        $respuesta = mysqli_query($conexion, "delete from eventos where id = " . $_POST['id']);
+    
+    case 'agregar':
+        $respuesta = mysqli_query($conexion, "insert into eventos (titulo, descripcion, inicio, fin, colortexto, colorfondo) values 
+        ('$_POST[titulo]','$_POST[descripcion]','$_POST[inicio]','$_POST[fin]',
+        '$_POST[colortexto]','$_POST[colorfondo]')");
         echo json_encode($respuesta);
         break;
 
-    case 'alumnos':
-        $datos = mysqli_query($conexion, "SELECT nombre, apellido FROM alumnos");
-        $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
-        echo json_encode($resultado);
-        break;
-
+        case 'modificar':
+            $respuesta = mysqli_query($conexion, "update eventos set titulo = '$_POST[titulo]', 
+            descripcion = '$_POST[descripcion]',
+            inicio =  '$_POST[inicio]',
+            fin = '$_POST[fin]',
+            colortexto = '$_POST[colortexto]',
+            colorfondo = '$_POST[colorfondo]' 
+            where id = '$_POST[id]'");
+            echo json_encode($respuesta);
+            break;
         
-}   
+
+    case 'borrar':
+        $respuesta = mysqli_query($conexion,"delete from eventos where id = $_POST[id] ");
+        echo json_encode($respuesta); 
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#switch($_GET['accion']) {
+ //Listar evento
+    #case 'listar':
+        #$datos = mysqli_query($conexion, "select id, 
+        #nombre_alumno as title,
+        #inicio as start,
+        #descripcion, 
+        #colortexto as textColor,
+        #colorfondo as backgroundColor
+        #from eventos");
+
+       # $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+        #echo json_encode($resultado);      
+        #break;
+
+ //Agregar evento
+    #case 'agregar':
+        
+        #$nombre_alumno = $_POST['nombre_alumno'];
+        #$inicio = $_POST['inicio'] . ' ' . $_POST['horario'] . ':00'; 
+        // Obtener la lista de materias separadas por comas
+    
+        #$respuesta = mysqli_query($conexion, "insert into eventos(nombre_alumno,inicio,descripcion,colortexto,colorfondo) values
+        #('$nombre_alumno','$inicio','$_POST[descripcion]',
+        #'$_POST[colortexto]','$_POST[colorfondo]')");
+        #echo json_encode($respuesta);    
+        #break;
+
+// Modificar evento
+#case 'modificar':
+    #$inicio = $_POST['inicio'] . ' ' . $_POST['horario'] . ':00';
+    #$inicio = date('Y-m-d H:i:s', strtotime($inicio));
+
+    #$respuesta = mysqli_query($conexion, "UPDATE eventos SET 
+        #nombre_alumno = '$_POST[nombre_alumno]',
+        #inicio = '$inicio', 
+        #descripcion = '$_POST[descripcion]',
+        #colortexto = '$_POST[colortexto]',
+        #colorfondo = '$_POST[colorfondo]'
+        #WHERE id = $_POST[id]");
+    #echo json_encode($respuesta);
+    #break;
+
+    //Borrar evento
+    #case 'borrar':
+        #$respuesta = mysqli_query($conexion, "delete from eventos where id = " . $_POST['id']);
+        #echo json_encode($respuesta);
+        #break;
+
+    #case 'alumnos':
+        #$datos = mysqli_query($conexion, "SELECT nombre, apellido FROM alumnos");
+        #$resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+        #echo json_encode($resultado);
+        #break;      
+#}   
 
 ?>
--- src/templates/agenda.php
+++ src/templates/agenda.php
@@ -14,6 +14,7 @@
 
 
   <!-- Scripts JS -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script src="/static/js/jquery-3.7.1.min.js"></script>
   <script src="/static/js/popper.min.js"></script>
   <script src="/static/js/bootstrap.min.js"></script>
@@ -21,6 +22,8 @@
   <script src="/static/js/bootstrap-clockpicker.js"></script>
   <script src="/static/js/moment-with-locales.js"></script>
   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.9/index.global.min.js'></script>
+
+
 
 </head>
 
@@ -68,13 +71,13 @@
           </li>
 
         </ul>
-
         <div class="text-center ">
           <a class="nav-link" href="{{ url_for('logout') }}">Cerrar Sesión</a>
         </div>
       </div>
     </div>
   </nav>
+
   <div class="container-fluid">
     <section class="content-header p-2">
       <h3 class="text-left subtitulo ">Calendario de clases</h3>
@@ -85,8 +88,422 @@
       </div>
     </div>
   </div>
-  |
   <!-- Formulario de eventos -->
+  <div class="modal fade" id="FormularioEventos" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close"
+            style="border:none; background-color:#ffffff; font-size:1.5em">
+            <span aria-hidden="true" class="cruz"> x </span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <input type="hidden" id="Id">
+          <div class="row mb-2">
+            <div class="form-group col">
+              <label for="">Nombre del alumno</label>
+              <input type="text" id="Titulo" class="form-control w-100" placeholder="">
+            </div>
+          </div>
+
+          <div class="row mb-2">
+            <div class="form-group col">
+              <label for="">Fecha de inicio</label>
+              <div class="input-group" data-autoclose="true">
+                <input type="date" id="FechaInicio" value="" class="form-control">
+              </div>
+            </div>
+            <div class="form-group col" id="TituloHoraInicio">
+              <label for="">Hora de inicio</label>
+              <div class="input-group " data-autoclose="true">
+                <input type="time" id="HoraInicio" class="form-control" value="" autocomplete="off">
+              </div>
+            </div>
+          </div>
+
+          <div class="row mb-2">
+            <div class="form-group col">
+              <label for="">Fecha de fin</label>
+              <div class="input-group" data-autoclose="true">
+                <input type="date" id="FechaFin" value="" class="form-control">
+              </div>
+            </div>
+            <div class="form-group col" id="TituloHoraFin">
+              <label for="">Hora de fin</label>
+              <div class="input-group" data-autoclose="true">
+                <input type="time" id="HoraFin" class="form-control" value="" autocomplete="off">
+              </div>
+            </div>
+          </div>
+          <div class="row mb-2">
+            <label for="Descripcion" class="col-form-label col-12">Descripción</label>
+            <div class="col-12">
+              <textarea name="Descripcion" id="Descripcion" class="form-control w-100" rows="3"></textarea>
+            </div>
+          </div>
+          <div class="row mb-2">
+            <div class="col">
+              <label for="">Color de fondo:</label>
+              <input type="color" value="#FBD374" id="ColorFondo" class="form-control" style="height:26px;">
+            </div>
+            <div class="col">
+              <label for="">Color de texto:</label>
+              <input type="color" value="#ffffff" id="ColorTexto" class="form-control" style="height:26px;">
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" id="BotonAgregar" class="btn-guardar">Agregar</button>
+            <button type="button" id="BotonModificar" class="btn-guardar"
+              style="background-color: #7ea56a">Modificar</button>
+            <button type="button" id="BotonBorrar" class="btn-guardar" style="background-color: #7ea56a">Borrar</button>
+            <button type="button" class="btn-guardar" style="background-color: #7ea56a"
+              data-bs-dismiss="modal">Cancelar</button>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <script>
+
+      let calendario1 = new FullCalendar.Calendar(document.getElementById('Calendario1'), {
+        height: 500,
+        headerToolbar: {
+          left: 'prev,next,today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        },
+        locale: 'es',
+        editable: true,
+        events: 'http://localhost/prueba/pruebaa/src/datoseventos.php?accion=listar',
+        dateClick: function (info) {
+          limpiarFormulario();
+          $('#BotonAgregar').show();
+          $('#BotonModificar').hide();
+          $('#BotonBorrar').hide();
+
+          if (info.allDay) {
+            $('#FechaInicio').val(info.dateStr);
+            $('#FechaFin').val(info.dateStr);
+          } else {
+            let fechaHora = info.dateStr.split("T");
+            $('#FechaInicio').val(fechaHora[0]);
+            $('#FechaFin').val(fechaHora[0]);
+            $('#HoraInicio').val(fechaHora[1].substring(0, 5));
+          }
+          $("#FormularioEventos").modal('show');
+        },
+
+        eventClick: function (info) {
+          $('#BotonAgregar').hide();
+          $('#BotonModificar').show();
+          $('#BotonBorrar').show();
+
+          $('#Id').val(info.event.id);
+          $('#Titulo').val(info.event.title);
+          $('#Descripcion').val(info.event.extendedProps.descripcion);
+          $('#FechaInicio').val(moment(info.event.start).format("YYYY-MM-DD"));
+          $('#FechaFin').val(moment(info.event.end).format("YYYY-MM-DD"));
+          $('#HoraInicio').val(moment(info.event.start).format("HH:mm"));
+          $('#HoraFin').val(moment(info.event.end).format("HH:mm"));
+          $('#ColorFondo').val(info.event.backgroundColor);
+          $('#ColorTexto').val(info.event.textColor);
+
+
+          $("#FormularioEventos").modal('show');
+        },
+        eventResize: function (info) {
+          $('#Id').val(info.event.id);
+          $('#Titulo').val(info.event.title);
+          $('#Descripcion').val(info.event.extendedProps.descripcion);
+          $('#FechaInicio').val(moment(info.event.start).format("YYYY-MM-DD"));
+          $('#FechaFin').val(moment(info.event.end).format("YYYY-MM-DD"));
+          $('#HoraInicio').val(moment(info.event.start).format("HH:mm"));
+          $('#HoraFin').val(moment(info.event.end).format("HH:mm"));
+          $('#ColorFondo').val(info.event.backgroundColor);
+          $('#ColorTexto').val(info.event.textColor);
+
+          let registro = recuperarDatosFormulario();
+          modificarRegistro(registro);
+        },
+        eventDrop: function (info) {
+          $('#Id').val(info.event.id);
+          $('#Titulo').val(info.event.title);
+          $('#Descripcion').val(info.event.extendedProps.descripcion);
+          $('#FechaInicio').val(moment(info.event.start).format("YYYY-MM-DD"));
+          $('#FechaFin').val(moment(info.event.end).format("YYYY-MM-DD"));
+          $('#HoraInicio').val(moment(info.event.start).format("HH:mm"));
+          $('#HoraFin').val(moment(info.event.end).format("HH:mm"));
+          $('#ColorFondo').val(info.event.backgroundColor);
+          $('#ColorTexto').val(info.event.textColor);
+
+          let registro = recuperarDatosFormulario();
+          modificarRegistro(registro);
+        },
+      });
+
+      calendario1.render();
+
+      //Eventos de botones
+      $('#BotonAgregar').click(function () {
+        let registro = recuperarDatosFormulario();
+        agregarRegistro(registro);
+        $('#FormularioEventos').modal('hide');
+      });
+
+      $('#BotonModificar').click(function () {
+        let registro = recuperarDatosFormulario();
+        modificarRegistro(registro);
+        $('#FormularioEventos').modal('hide');
+      });
+
+      $('#BotonBorrar').click(function () {
+        let registro = recuperarDatosFormulario();
+        borrarRegistro(registro);
+        $('#FormularioEventos').modal('hide');
+      });
+
+      //Funciones para comunicarse con el servidor AJAX
+      function agregarRegistro(registro) {
+        $.ajax({
+          type: 'POST',
+          url: 'http://localhost/prueba/pruebaa/src/datoseventos.php?accion=agregar',
+          data: registro,
+          success: function (msg) {
+            calendario1.refetchEvents();
+            mostrarSweetAlert("¡Alumno agregado correctamente!", "El alumno ha sido agregado exitosamente.", "success");
+            $('#FormularioEventos').modal('hide'); // Esto oculta el modal después de agregar
+          },
+          error: function (error) {
+            mostrarSweetAlert("Error", "Hubo un error al agregar el evento.", "error");
+            console.log("Hubo un error al agregar el evento:", error);
+          }
+        });
+      }
+
+
+      function modificarRegistro(registro) {
+        $.ajax({
+          type: 'POST',
+          url: 'http://localhost/prueba/pruebaa/src/datoseventos.php?accion=modificar',
+          data: registro,
+          success: function (msg) {
+            calendario1.refetchEvents();
+            mostrarSweetAlert("¡Cambios guardados correctamente!", "Los cambios han sido guardados exitosamente.", "success");
+            $('#FormularioEventos').modal('hide'); // Esto oculta el modal después de modificar
+          },
+          error: function (error) {
+            mostrarSweetAlert("Error", "Hubo un error al modificar el evento.", "error");
+            console.log("Hubo un error al modificar el evento:", error);
+          }
+        });
+      }
+
+      function mostrarSweetAlert(title, text, icon) {
+        Swal.fire({
+          title: title,
+          text: text,
+          icon: icon,
+          confirmButtonText: "Aceptar",
+        });
+      }
+
+      function borrarRegistro(registro) {
+        $.ajax({
+          type: 'POST',
+          url: 'http://localhost/prueba/pruebaa/src/datoseventos.php?accion=borrar',
+          data: registro,
+          success: function (msg) {
+            calendario1.refetchEvents();
+            mostrarSweetAlert("¡Alumno eliminado correctamente!", "El alumno ha sido eliminado exitosamente.", "success");
+          },
+          error: function (error) {
+            mostrarSweetAlert("Error", "Hubo un error al eliminar la clase.", "error");
+            console.log("Hubo un error al borrar el evento:", error);
+          }
+        });
+      }
+
+
+      //Funciones que interactuan con el formulario eventos
+      function limpiarFormulario() {
+        $('#Id').val('');
+        $('#Titulo').val('');
+        $('#Descripcion').val('');
+        $('#FechaFin').val('');
+        $('#FechaInicio').val('');
+        $('#HoraInicio').val('');
+        $('#HoraFin').val('');
+        $('#ColorFondo').val('#FBD374');
+        $('#ColorTexto').val('#ffffff');
+      }
+
+      function recuperarDatosFormulario() {
+        let fechaInicio = $('#FechaInicio').val();
+        let horaInicio = $('#HoraInicio').val();
+        let fechaFin = $('#FechaFin').val();
+        let horaFin = $('#HoraFin').val();
+        // Formatear fecha y hora correctamente
+        let inicio = moment(`${fechaInicio} ${horaInicio}`, 'YYYY-MM-DD HH:mm').format();
+        let fin = moment(`${fechaFin} ${horaFin}`, 'YYYY-MM-DD HH:mm').format();
+
+
+        let registro = {
+          id: $('#Id').val(),
+          titulo: $('#Titulo').val(),
+          descripcion: $('#Descripcion').val(),
+          inicio: inicio,
+          fin: fin,
+          colorfondo: $('#ColorFondo').val(),
+          colortexto: $('#ColorTexto').val()
+        }
+        return registro;
+      }
+
+    </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Formulario de eventos 
   <div class="modal fade" id="FormularioEventos" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-md" role="document">
       <div class="modal-content">
@@ -312,7 +729,7 @@
       };
       return registro;
     }
-  </script>
+  </script>-->
 
 
 </body>
